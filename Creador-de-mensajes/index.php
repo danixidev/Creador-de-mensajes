16f30fded88eb79b73f7e349d6563ec223925c71
Dani <92784534+danixidev@users.noreply.github.com>
2021-10-20 15:20:22 -0700

    Add files via upload

---
+++ Creador-de-mensajes/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="">
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name" value="John"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
