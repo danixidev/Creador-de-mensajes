d0b331b85660a6d8b3432f880a0e73553e92f9d4
Dani <92784534+danixidev@users.noreply.github.com>
2021-10-20 15:20:44 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 <h2>HTML Forms</h2>
 
 <form action="">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name" value="John"><br>
+  <label for="password">Password:</label><br>
+  <input type="password" id="password" name="password" value=""><br><br>
   <input type="submit" value="Submit">
 </form> 
 
