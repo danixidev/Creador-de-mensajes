a60b704f22eeb04ea6f9e8c9501dcd2228292cac
Dani <danixidev@gmail.com>
2021-10-24 18:18:52 +0200

    Created proper HTML structure

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Creador de mensajes</title>
+</head>
 <body>
+  
+  <header>
+    <form action="" method="POST">
+      <input type="text" id="message" name="message" placeholder="Your message">
+      <input type="submit" value="Submit">
+    </form> 
+  </header>
 
-<h2>HTML Forms</h2>
+  <article>
 
-<form action="">
-  <label for="name">Name:</label><br>
-  <input type="text" id="name" name="name" value="John"><br>
-  <label for="password">Password:</label><br>
-  <input type="password" id="password" name="password" value=""><br><br>
-  <input type="submit" value="Submit">
-</form> 
 
+
+  </article>
 </body>
 </html>
+
+
+<?php 
+
+if(isset($_POST['message'])) {
+
+  echo($_POST['message']);
+
+}
+
+?>
